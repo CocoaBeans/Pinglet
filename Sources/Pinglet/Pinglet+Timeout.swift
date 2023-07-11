f9d762d14927b028147ca83e064c772b822b663c
Kevin Ross <kevin@pingpoint.io>
2023-07-11 18:48:29 -0400

    Use default mode instead to avoid contention with UI scrolling

--- Sources/Pinglet/Pinglet+Timeout.swift
+++ Sources/Pinglet/Pinglet+Timeout.swift
@@ -74,7 +74,7 @@ extension Pinglet {
 
             // If we have an internal socket run loop then we will add the timer to that run loop.
             if let cfRunLoop = self.socket?.runLoop {
-                CFRunLoopAddTimer(cfRunLoop, timer, .commonModes)
+                CFRunLoopAddTimer(cfRunLoop, timer, .default)
             }
             else {
                 RunLoop.main.add(timer, forMode: .default)
